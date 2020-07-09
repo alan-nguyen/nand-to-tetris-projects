f0b5b29a642723324634c2817f3a071775c2405c
Alan Nguyen <tuan8101@gmail.com>
2020-07-09 00:21:07 -0600

    Fix bugs in mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,58 +7,61 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Pseudo code
-// Compute R2 = R0 + R0 for R1 times
-// n = R1
+// Compute R2 += R0 for R1 times
+// x = R0
+// y = R1
 // i = 1
-// mul = R0
+// mult = R2
 // IF R0 or R1 = 0
 //  R2 = 0
 // ELSE
 //  LOOP
-//   if i > n goto STOP
-//   mul = mul + mul
+//   if i > y goto STOP
+//   mult = mult + x
 //   i = i + 1
 //   goto LOOP
 //  STOP:
-//    R2 = mul
+//    R2 = mult
 
   @R1
   D=M
   @POSITIVE
   D; JEQ   // If R1 = 0 goto POSITIVE
-  @n
-  M=D   // n = R1
+  @y
+  M=D   // y = R1
   @i
   M=1   // i = 1
   @R0
   D=M
   @POSITIVE
   D; JEQ   // If R0 = 0 goto POSITIVE
-  @mul
-  M=D   // mul = R0
+  @x
+  M=D   // x = R0
+  @mult
+  M=0   // mult = 0
 (LOOP)
   @i
   D=M
-  @n
-  D=D-M   // D[i] - D[n]
+  @y
+  D=D-M   // D[i] - D[y]
   @STOP
-  D;JGT   // i > n goto STOP
-  @mul
+  D;JGT   // i > x goto STOP
+  @x
   D=M
-  @mul
-  M=D+M   // mul = mul + mul
+  @mult
+  M=D+M   // mult = mult + x
   @i
   M=M+1   // i = i + 1
   @LOOP
   0;JMP
-(STOP)
-  @mul
-  D=M
-  @R2
-  M=D   // RAM[2] = mul
 (POSITIVE)
   @R2
   M=0
+(STOP)
+  @mult
+  D=M
+  @R2
+  M=D   // RAM[2] = mult
 (END)
   @END
   0;JMP
