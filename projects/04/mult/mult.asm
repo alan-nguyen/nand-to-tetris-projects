8be7178477ac237f2dd6a1051197b554e438aa76
Alan Nguyen <tuan8101@gmail.com>
2020-07-08 23:24:44 -0600

    Create mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,53 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Pseudo code
+// Compute R2 = R0 + R0 for R1 times
+// n = R1
+// i = 1
+// mul = R0
+// IF R0 or R1 = 0
+//  R2 = 0
+// ELSE
+//  LOOP
+//   if i > n goto STOP
+//   mul = mul + mul
+//   i = i + 1
+//   goto LOOP
+//  STOP:
+//    R2 = mul
+
+  @R1
+  D=M
+  @n
+  M=D   // n = R1
+  @i
+  M=1   // i = 1
+  @R0
+  D=M
+  @mul
+  M=D   // mul = R0
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M   // D[i] - D[n]
+  @STOP
+  D;JGT   // i > n goto STOP
+  @mul
+  D=M
+  @mul
+  M=D+M   // mul = mul + mul
+  @i
+  M=M+1   // i = i + 1
+  @LOOP
+  0;JMP
+(STOP)
+  @mul
+  D=M
+  @R2
+  M=D   // RAM[2] = mul
+(END)
+  @END
+  0;JMP
+
